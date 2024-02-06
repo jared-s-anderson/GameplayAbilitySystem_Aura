16c806ab32a60f536454d65bdb3d92d62b873419
Jared Anderson <jaredsa@byui.edu>
2024-02-06 13:25:44 -0700

    Player Level & Combat Interface Created

--- Source/Aura/Public/Character/AuraEnemy.h
+++ Source/Aura/Public/Character/AuraEnemy.h
@@ -21,7 +21,14 @@ public:
 	virtual void HighlightActor() override;
 	virtual void UnHighlightActor() override;
 	/** End of Enemy Interface */
+
+	/** Combat Interface */
+	virtual int32 GetPlayerLevel() override;
+	/** End of Combat Interface */
 protected:
 	virtual void BeginPlay() override;
 	virtual void InitAbilityActorInfo() override;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Character Class Defaults")
+	int32 Level = 1;
 };
